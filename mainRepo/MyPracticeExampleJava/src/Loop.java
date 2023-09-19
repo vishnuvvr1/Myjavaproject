a12444f384839f5592bcc3e708d413ead517b181
vishnuvvr1 <vishnuvvr8919@gmail.com>
2023-09-19 15:54:22 +0530

    "loops example one commit"

---
+++ mainRepo/MyPracticeExampleJava/src/Loop.java
@@ -0,0 +1,10 @@
+public class Loop {
+    public static void main(String[] args) {
+        int n, i;
+        n=2;
+        for (i=1;i<=10;i++)
+        {
+            System.out.println(n+"*"+i+"="+n*i);
+        }
+    }
+}

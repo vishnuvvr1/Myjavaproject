7e954dba6a819b114adbbec63676c2c670f463a7
vishnuvvr1 <vishnuvvr8919@gmail.com>
2023-09-19 11:15:25 +0530

    "my overloading method commit"

--- mainRepo/sub/myproj.java
+++ mainRepo/sub/myproj.java
@@ -1,8 +1,31 @@
- public class Myclass {
-     public static void main(String[] args) {
-         System.out.println("MY class");
-         System.out.println("My name is vishnu");
-         System.out.println("My name is gowtham");
-         System.out.println("my name is saif");
+public class Myclass {
+         public void add(int a, int y)
+         {
+
+             System.out.println(a + y);
+
+         }
+
+         public void von(int x, int y, int z)
+         {
+             System.out.println(x + y + z);
+
+         }
+
+         public void doub(double x, double y)
+         {
+             System.out.println(x + y);
+         }
+
+         public void str(String s, String d) {
+             System.out.println(s + d);
+         }
+         public static void main (String[]args){
+             Myclass a1 = new Myclass();
+             a1.add(10, 20);
+             a1.von(10, 20, 56);
+             a1.doub(20.1, 2014.1);
+             a1.str("vishnu", "safi");
+
+         }
      }
-        }

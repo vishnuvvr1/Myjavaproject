a1dd47820fb1f5b79033e57462642a7571a45fe5
vishnuvvr1 <vishnuvvr8919@gmail.com>
2023-09-21 14:57:21 +0530

    "loop table code examples commit"

--- mainRepo/MyPracticeExampleJava/src/Loop.java
+++ mainRepo/MyPracticeExampleJava/src/Loop.java
@@ -1,6 +1,6 @@
 public class Loop {
     public static void main(String[] args) {
-        for (int i = 0; i <= 5; i++) {
+        for (int i = 0; i >= 5; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
---
+++ mainRepo/MyPracticeExampleJava/src/LoopExample2.java
@@ -0,0 +1,9 @@
+public class LoopExample2 {
+    public static void main(String[] args) {
+        int i, n;
+        n = 2;
+        for (i = 1; i <= 10; i++) {
+            System.out.println(n + "*" + i + "=" + n * i);
+        }
+    }
+}

8e71c3bda9573b1c2760f099a9ceaf95021d142f
vishnuvvr1 <vishnuvvr8919@gmail.com>
2023-09-21 14:49:03 +0530

    "loop pattern examples commit"

--- mainRepo/MyPracticeExampleJava/src/Loop.java
+++ mainRepo/MyPracticeExampleJava/src/Loop.java
@@ -1,10 +1,11 @@
 public class Loop {
     public static void main(String[] args) {
-        int n, i;
-        n=2;
-        for (i=1;i<=10;i++)
-        {
-            System.out.println(n+"*"+i+"="+n*i);
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
+
     }
 }

86ca57eb47762ea132c23f906f254980e1569719
vishnuvvr1 <vishnuvvr8919@gmail.com>
2023-09-21 14:03:02 +0530

    "if male or female example commit"

--- mainRepo/MyPracticeExampleJava/src/ifConditionExamples/IfConditionExample_2.java
+++ mainRepo/MyPracticeExampleJava/src/ifConditionExamples/IfConditionExample_2.java
@@ -11,25 +11,20 @@ public class IfConditionExample_2 {
         String str1 = sc.nextLine();
         System.out.println(str1 + "Enter age : ");
         int a = sc.nextInt();
-        if(str1.equalsIgnoreCase("m") || str1.equalsIgnoreCase("male")){
-
-        }
-        if (a > 18) {
-            System.out.println("hiboy");
-        } else if (a < 18) {
-            System.out.println("hi man");
+        if (str1.equalsIgnoreCase("m") || str1.equalsIgnoreCase("male")) {
+            if (a > 18) {
+                System.out.println("hi boy");
+            } else if (a < 18) {
+                System.out.println("hi man");
+            }
         } else if (str1.equalsIgnoreCase("F") || str1.equalsIgnoreCase("female")) {
-
-        } else if (a > 18) {
-            System.out.println("hi girl");
-
-        } else if (a > 18) {
-            System.out.println("hi woman");
-
-        }
-        else {
+            if (a > 18) {
+                System.out.println("hi girl");
+            } else if (a < 18) {
+                System.out.println("hi woman");
+            }
+        } else {
             System.out.println("please enter correct gender (M or f) or (male female)");
         }
     }
-
 }

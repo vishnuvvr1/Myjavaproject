8a70c1267e6cec66ac548420ea5b8ebe848f432d
vishnuvvr1 <vishnuvvr8919@gmail.com>
2023-09-25 16:32:25 +0530

    "Array nexample prg commit"

---
+++ mainRepo/MyPracticeExampleJava/src/Array_1.java
@@ -0,0 +1,10 @@
+    import java.util.List;
+public class Array_1 {
+    public static void main(String[] args) {
+        int[] arrayList = {10, 50, 30, 40, 20};
+        for (int i = 0; i< arrayList.length; i++) {
+            System.out.println(arrayList[i]);
+        }
+        System.out.println("element at first index: " + arrayList[1]);
+    }
+}
--- mainRepo/MyPracticeExampleJava/src/InternalExams/Currentbill.java
+++ mainRepo/MyPracticeExampleJava/src/InternalExams/Currentbill.java
@@ -6,13 +6,13 @@ public class Currentbill
 {
     public double cal(int u) {
         double amt = 0;
-        if (u <= 100) {
+        if (u < 100) {
             amt = 1.25 * u;
         }
-        else if (u <= 200) {
+        else if (u < 200) {
             amt = 1.25 * 100 + (u - 100) * 1.5;
         }
-        else {
+        else if(u < 300) {
             amt = 1.25 * 100 + 1.5 * 100 + (u - 200) * 1.8;
         }
         return amt;
@@ -26,10 +26,10 @@ public class Currentbill
         System.out.print("Enter Consumer No.: ");
         int cNo = in.nextInt();
         System.out.print("Enter present reading: ");
-        int curr = in.nextInt();
+        int present = in.nextInt();
         System.out.print("Enter previous reading: ");
         int prev = in.nextInt();
-        int used = curr - prev;
+        int used = present - prev;
         double amount = obj.cal(used);
         System.out.println("Consumer No.\tName\tUnits Consumed\tAmount");
         System.out.println(cNo + "\t\t" + name + "\t" + used + "\t" + amount);

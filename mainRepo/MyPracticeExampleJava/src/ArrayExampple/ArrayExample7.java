9f5f0f6854f92ac7dc3595b9fb7c098130f40f22
vishnuvvr1 <vishnuvvr8919@gmail.com>
2023-09-26 18:13:25 +0530

    "array sum all elements example prg commit"

---
+++ mainRepo/MyPracticeExampleJava/src/ArrayExampple/ArrayExample7.java
@@ -0,0 +1,23 @@
+package ArrayExampple;
+
+import java.util.Scanner;
+
+public class ArrayExample7 {
+    public static void main(String[] args) {
+        Scanner vvr = new Scanner(System.in);
+        int[] array = new int[5];
+        int i;
+        int sum = 0;
+        for (i = 0; i < array.length; i++) {
+            System.out.println("enter a five number=");
+            int a = vvr.nextInt();
+            array[i] = a;
+        }
+        for (int var : array) {
+            sum = sum+var;
+
+            System.out.println("sum of all numbers=" + var + " ");
+        }
+        System.out.println("sum of given all numbers=" +sum);
+    }
+}

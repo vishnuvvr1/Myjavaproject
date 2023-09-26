19bdff81fe60fbe3b1a00e4c02b60850f357084a
vishnuvvr1 <vishnuvvr8919@gmail.com>
2023-09-26 16:48:39 +0530

    Array Examples prg commit"

---
+++ mainRepo/MyPracticeExampleJava/src/Array_2.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class Array_2 {
+
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        //System.out.println(cars);
+        //cars.remove("Volvo");
+        //cars.add("bmw2");
+        cars.clear();
+        cars.isEmpty();
+        System.out.println(cars);
+    }
+}

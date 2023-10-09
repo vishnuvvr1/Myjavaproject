877a7aeb889dabd3e3321a759d2fb130a28d80bd
Saifuddin10 <145104890+Saifuddin10@users.noreply.github.com>
2023-10-09 15:11:19 +0530

    Update ArrayExample13.java

--- mainRepo/MyPracticeExampleJava/src/ArrayExampple/ArrayExample13.java
+++ mainRepo/MyPracticeExampleJava/src/ArrayExampple/ArrayExample13.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class ArrayExample13 {
     public static void main(String[] args) {
         int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}};
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                System.out.print(array[i][j] + " ");
+        for (int l = 0; l < array.length; l++) {
+            for (int m = 0; m < array.length; m++) {
+                System.out.print(array[l][m] + " ");
             }
             System.out.println();
         }
